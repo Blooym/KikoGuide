05ed8fa0e936afbab3e819f261644516e5456e5b
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-11-08 01:48:16 +0000

    chore: add translation for 'unknown'

--- KikoGuide/UI/Windows/GuideViewer/GuideViewer.presenter.cs
+++ KikoGuide/UI/Windows/GuideViewer/GuideViewer.presenter.cs
@@ -30,7 +30,7 @@ namespace KikoGuide.UI.Windows.GuideViewer
         /// <summary>
         ///     Gets the current plugin version.
         /// </summary>
-        internal static string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+        internal static string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? TGenerics.;
 
         /// <summary>
         ///     Pulls the configuration from the plugin service.
